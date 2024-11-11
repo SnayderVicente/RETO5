d544a579ceac9f721a4347d4db62c3398a10ee20
Snayder Vicente Conca <132237250+SnayderVicente@users.noreply.github.com>
2024-11-11 12:35:47 -0500

    Add files via upload

---
+++ RETO_CUA/script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {  
+    // Oculta la pantalla de carga  
+    const loadingScreen = document.getElementById('loading-screen');  
+    loadingScreen.style.display = 'none';  
+});  
